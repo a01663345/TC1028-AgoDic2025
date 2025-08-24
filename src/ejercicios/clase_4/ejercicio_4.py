3693fdb3db9d2fb9a980da26252cbfe754122666
a01663345 <a01663345@tec.mx>
2025-08-24 01:38:27 -0600

    Update ejercicio_4.py

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,10 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+num  = float(input('Introduzca su número: '))
+if num < 0:
+  print('Negativo')
+elif num == 0:
+  print('Cero')
+else:
+  print('Positivo')
