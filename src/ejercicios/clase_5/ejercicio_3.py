1bba742f8c3d87944780405a625892699ffa3c60
a01663345 <a01663345@tec.mx>
2025-08-26 21:19:29 -0600

    A01663345_ejercicio_3_clase5.py

--- src/ejercicios/clase_5/ejercicio_3.py
+++ src/ejercicios/clase_5/ejercicio_3.py
@@ -39,3 +39,36 @@ Ejemplos de ejecución del programa
 >>>m
 20
 """
+
+def suma (num_1: int, num_2: int) -> int:
+  return num_1 + num_2
+  
+def resta (num_1: int, num_2: int) -> int:
+  return num_1 - num_2
+
+def multiplicacion (num_1: int, num_2: int) -> int:
+  return num_1 * num_2
+
+def division (num_1: int, num_2: int) -> float:
+  return num_1 / num_2
+
+def main ():
+  numero_1 = int(input('Ingresa tu primer número: '))
+  numero_2 = int(input('Ingresa tu segundo número: '))
+  clave = input('Clave: ')
+
+  if clave == "s":
+    print(f'La suma de ambos números es {suma ( num_1 = numero_1, num_2 = numero_2)}')
+  elif clave == "r":
+        print(f'La resta de ambos números es {resta ( num_1 = numero_1, num_2 = numero_2)}')
+  elif clave == "m":
+        print(f'La multiplicación de ambos números es {multiplicacion ( num_1 = numero_1, num_2 = numero_2)}')
+  elif clave == "d":
+    if numero_2 == 0:
+        print("Error: no se puede dividir entre cero")
+    else:
+        print(f'La división de ambos números es {division(num_1=numero_1, num_2=numero_2)}')
+  else:
+    print('Clave inválida')
+  
+main()
