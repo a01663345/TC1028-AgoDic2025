100cbfc426f5eb98cb723dcc82ae545ea629b9f4
a01663345 <a01663345@tec.mx>
2025-09-10 21:02:52 -0600

    A01663345_ejercicio_5.py

--- src/ejercicios/clase_9/ejercicio_5.py
+++ src/ejercicios/clase_9/ejercicio_5.py
@@ -1,3 +1,10 @@
 """
 Imprime un cuadrado de 5x5 usando el carácter asterisco (*).
 """
+def main():
+    for i in range(5):
+        for j in range(5):
+            print("*", end="")
+        print()
+
+main()
